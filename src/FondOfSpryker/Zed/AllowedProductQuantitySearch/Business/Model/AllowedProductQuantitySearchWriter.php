6a5e91d28c333fee34c9d115b0d1927382f6fde1
Markus Nörenberg <markus.noerenberg@fondofbags.com>
2019-07-29 12:19:38 +0200

    comply to the interface

--- src/FondOfSpryker/Zed/AllowedProductQuantitySearch/Business/Model/AllowedProductQuantitySearchWriter.php
+++ src/FondOfSpryker/Zed/AllowedProductQuantitySearch/Business/Model/AllowedProductQuantitySearchWriter.php
@@ -4,4 +4,21 @@ namespace FondOfSpryker\Zed\AllowedProductQuantitySearch\Business\Model;
 
 class AllowedProductQuantitySearchWriter implements AllowedProductQuantitySearchWriterInterface
 {
+    /**
+     * @param array $allowedProductQuantityIds
+     *
+     * @return void
+     */
+    public function publish(array $allowedProductQuantityIds)
+    {
+    }
+
+    /**
+     * @param array $allowedProductQuantityIds
+     *
+     * @return void
+     */
+    public function unpublish(array $allowedProductQuantityIds)
+    {
+    }
 }
